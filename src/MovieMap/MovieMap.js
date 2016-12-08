e59998f806bb5d719e55449ac099c3c42d0f2f65
Daniel Spitzer <dsp@tradeshift.com>
2016-12-08 21:53:08 +0100

    [Fail] tried to make react, mapgl and webpack work

--- src/MovieMap/MovieMap.js
+++ src/MovieMap/MovieMap.js
@@ -1,26 +1,68 @@
-import React, { PropTypes } from 'react';
-import React, { Component, PropTypes } from 'react';
-import { Link } from 'react-router';
-import { MoviePropType } from '../MovieCard/MovieCard';
+import React, {Component} from 'react';
+// import DeckGL from 'deck.gl/react';
+// import {LineLayer} from 'deck.gl';
+import MapGL from 'react-map-gl';
 
-const MovieMap = ({locations}) => (
-	<div>
-		movie map
-		<Link to="/">list</Link>
-	</div>
-);
+const token = 'pk.eyJ1Ijoic2FtcGkiLCJhIjoiY2l3NWFhaWhoMDAweTJ0bzR6YzNnbjFoNiJ9.gfuh134ea9lH6Fy6mNkJkA';
 
-MovieMap.propTypes = {
-	movie: MoviePropType,
-	locations: PropTypes.arrayOf(PropTypes.shape({
-		id: PropTypes.string.isRequired,
-		movieId: PropTypes.string.isRequired,
-		origAddress: PropTypes.string.isRequired,
-		funFacts: PropTypes.string,
-		address: PropTypes.string,
-		lat: PropTypes.number,
-		lon: PropTypes.number
-	})).isRequired
-};
+export default class MovieMap extends Component {
 
-export default MovieMap;
+  // state = {
+  //   viewport: {
+  //     latitude: 37.785164,
+  //     longitude: -122.41669,
+  //     zoom: 16.140440,
+  //     bearing: -20.55991,
+  //     pitch: 60,
+  //   },
+  //   width: 500,
+  //   height: 500,
+  // }
+
+  render() {
+
+		return (
+			<MapGL
+				width={400}
+				height={400}
+				latitude={37.7577}
+				longitude={-122.4376}
+				zoom={8}
+				mapboxApiAccessToken={ token }
+				onChangeViewport={viewport => {
+					const {latitude, longitude, zoom} = viewport;
+					// Optionally call `setState` and use the state to update the map.
+				}}
+			/>
+		);
+
+    // const {viewport, width, height} = this.state;
+
+    // const layers = [new LineLayer({
+    //   data: [{
+    //     sourcePosition: [-122.41669, 37.7853],
+    //     targetPosition: [-122.41669, 37.781],
+    //   }],
+    // })];
+		//
+    // return (
+    //   <MapGL
+    //     {...viewport}
+    //     mapStyle="mapbox://styles/mapbox/dark-v9"
+    //     onChangeViewport={v => this.setState({viewport: v})}
+    //     preventStyleDiffing={false}
+    //     mapboxApiAccessToken={token}
+    //     perspectiveEnabled
+    //     width={width}
+    //     height={height}>
+    //     <DeckGL
+    //       {...viewport}
+    //       width={width}
+    //       height={height}
+    //       layers={layers}
+    //       debug />
+    //   </MapGL>
+    // );
+  }
+
+}
